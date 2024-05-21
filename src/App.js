f0f8afcc40d3ae1a9d19e71173f641512e4f73d5
mridul-pr <mridulpramod8@gmail.com>
2024-05-21 22:07:42 +0530

    Added text and background for each page

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ import Details_Villa from './components/pages/Details_Villa';
 import Details_Apartment from './components/pages/Details_Apartment';
 import Details_Condominium from './components/pages/Detail_Condominium';
 import Details_Row from './components/pages/Details_Row';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
+        <Navbar/>
         <Routes>
           <Route path='/' exact Component = {Home}/>
           <Route path='/ContactUs' exact Component = {ContactUs}/>
@@ -24,6 +25,7 @@ function App() {
           <Route path='/Details_Condominium' exact Component={Details_Condominium}/>
           <Route path='/Details_Row' exact Component={Details_Row}/>
         </Routes>
+        <Footer/>
       </Router>
     </>
   );
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,28 +1,30 @@
 import React from 'react';
+import { Link as ScrollLink } from 'react-scroll';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
-import Video from '../videos/video-1.mp4'
+import Video from '../videos/video-1.mp4';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-    <video src={Video} className='video' autoPlay loop muted />
+      <video src={Video} className='video' autoPlay loop muted />
       <h1>Engineer & Builders</h1>
       <p>Defence Contract</p>
       <div className='hero-btns'>
-        <Button
+        <ScrollLink
+          to='portfolios'
+          smooth={true}
+          duration={500}
+          offset={-80}
           className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
         >
           Services
-        </Button>
+        </ScrollLink>
         <Button
           className='btns'
           buttonStyle='btn--primary'
           buttonSize='btn--large'
-          onClick={console.log('hey')}
         >
           Contact Us <i className='far fa-play-circle' />
         </Button>
