081295ccf1bffb6daf82d72fd1b96cc0f283f0e8
mridul-pr <mridulpramod8@gmail.com>
2024-05-24 14:55:11 +0530

    small changes to text

--- src/components/pages/Details_Residence.js
+++ src/components/pages/Details_Residence.js
@@ -9,7 +9,7 @@ export default function DetailsResidence() {
             <h2 id="building-name">Residentials</h2>
             <p id="building-description">Welcome to Atom Residentials, where modern architecture meets unparalleled comfort. Our state-of-the-art residential building is designed with meticulous attention to detail, offering spacious, elegantly designed apartments that cater to your every need. Located in a prime area, our building features top-notch amenities, including a fitness center, rooftop garden, and 24/7 security, ensuring a safe and vibrant community for all our residents. Experience the perfect blend of luxury and convenience at Atom Residentials, your ideal home in the heart of the city.</p>
             </div>
-         </div>
+            </div>
   )
 };
 
